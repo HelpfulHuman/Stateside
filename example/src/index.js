d1386b181be5c9a855328a922740d0e2c7769190
Nicholas Glenn <nick@helpfulhuman.com>
2017-09-19 01:11:27 -0700

    Updating the example app to provide a more robust use case

--- example/src/index.js
+++ example/src/index.js
@@ -3,12 +3,12 @@ import * as ReactDOM from "react-dom";
 import { Router, Link, withRedirect } from "stateside";
 
 
-
 class App extends React.PureComponent {
   constructor (props, context) {
     super(props, context);
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
+    this.handleGreeting = this.handleGreeting.bind(this);
     this.state = { loggedIn: false };
   }
 
@@ -20,23 +20,29 @@ class App extends React.PureComponent {
     this.setState({ loggedIn: false });
   }
 
-  render () {
-    var loggedIn = this.state.loggedIn;
+  handleGreeting (name) {
+    Router.to(`/greet/${name}`);
+  }
 
+  render () {
     return (
       <div>
         <Router
-          loggedIn={loggedIn}
+          loggedIn={this.state.loggedIn}
           component={GuestOnly}>
           <Login route="/login" onLogin={this.login} />
           <NotFound defaultRoute />
         </Router>
         <Router
           onLogout={this.logout}
-          loggedIn={loggedIn}
+          loggedIn={this.state.loggedIn}
           component={LoggedInOnly}>
           <Home route="/" />
-          <About route="/about" />
+          <Greet route="/greet/:name" />
+          <GreetForm
+            route="/greet"
+            partialRoute
+            onGreet={this.handleGreeting} />
           <NotFound defaultRoute />
         </Router>
       </div>
@@ -47,32 +53,32 @@ class App extends React.PureComponent {
 
 const GuestOnly = withRedirect(function (props) {
   return (props.loggedIn ? "/" : null);
-})(function (props) {
-  return (<div>{props.children}</div>);
+})(function ({ children }) {
+  return (<div>{children}</div>);
 });
 
 
 const LoggedInOnly = withRedirect(function (props) {
   return (!props.loggedIn ? "/login" : null);
-})(function (props) {
+})(function ({ onLogout, children }) {
   return (
     <div>
       <nav>
         <Link to="/" activeClassName="isActive">Home</Link>
-        <Link to="/about" activeClassName="isActive">About</Link>
-        <a onClick={props.onLogout}>Logout</a>
+        <Link to="/greet" activeClassName="isActive">Greet</Link>
+        <a onClick={onLogout}>Logout</a>
       </nav>
-      {props.children}
+      {children}
     </div>
   );
 });
 
 
-function Login (props) {
+function Login ({ onLogin }) {
   return (
     <div>
       Welcome!  Please log in...
-      <a onClick={props.onLogin}>Login</a>
+      <a onClick={onLogin}>Login</a>
     </div>
   );
 }
@@ -85,10 +91,32 @@ function Home (props) {
 }
 
 
-function About (props) {
+function Greet ({ params }) {
+  var name = (params.name || "World");
   return (
-    <h1>About Page</h1>
+    <h1>Hello, {name}!</h1>
   );
+}
+
+
+class GreetForm extends React.PureComponent {
+  constructor (props, context) {
+    super(props, context);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick () {
+    this.props.onGreet(this.nameInput.value);
+  }
+
+  render () {
+    return (
+      <div>
+        <input type="text" ref={c => this.nameInput = c} placeholder="Enter Your Name" />
+        <button onClick={this.handleClick}>Greet</button>
+      </div>
+    );
+  }
 }
 
 
